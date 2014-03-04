c2de198480090c272d6afc3182dff7a83efbc29f
jsteemann <j.steemann@triagens.de>
2014-03-04 11:07:18 +0100

    adjusted setup script

--- tests/travis/setup_arangodb.sh
+++ tests/travis/setup_arangodb.sh
@@ -3,7 +3,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
 
-VERSION=1.4.0
+VERSION=2.0.0-rc1
 NAME=ArangoDB-$VERSION
 
 if [ ! -d "$DIR/$NAME" ]; then
@@ -19,7 +19,11 @@ PID=$(echo $PPID)
 TMP_DIR="/tmp/arangodb.$PID"
 PID_FILE="/tmp/arangodb.$PID.pid"
 ARANGODB_DIR="$DIR/$NAME"
-ARANGOD="${ARANGODB_DIR}/bin/arangod_x86_64"
+
+ARANGOD="${ARANGODB_DIR}/bin/arangod"
+if [ "$ARCH" == "x86_64" ]; then
+  ARANGOD="${ARANGOD}_x86_64"
+fi
 
 # create database directory
 mkdir ${TMP_DIR}
@@ -32,12 +36,8 @@ ${ARANGOD} \
     --server.endpoint tcp://127.0.0.1:8529 \
     --javascript.app-path ${ARANGODB_DIR}/js/apps \
     --javascript.startup-directory ${ARANGODB_DIR}/js \
-    --javascript.modules-path ${ARANGODB_DIR}/js/server/modules:${ARANGODB_DIR}/js/common/modules:${ARANGODB_DIR}/js/node \
-    --javascript.package-path ${ARANGODB_DIR}/js/npm:${ARANGODB_DIR}/js/common/test-data/modules \
-    --javascript.action-directory ${ARANGODB_DIR}/js/actions \
     --database.maximal-journal-size 1048576 \
-    --server.disable-authentication true \
-    --javascript.gc-interval 1 &
+    --server.disable-authentication true &
 
 sleep 2
 
